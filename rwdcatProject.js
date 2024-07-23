f7e6a1a7394f55ee584053de014f893bc738135e
kitty841002@gmai.com <kitty841002@gmai.com>
2024-07-23 23:53:37 +0800

    修改BUG

--- rwdcatProject.js
+++ rwdcatProject.js
@@ -9,29 +9,51 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  
+  if (n > x.length) {
+    slideIndex = 1;
+  } else if (n < 1) {
+    slideIndex = x.length;
+  } else {
+    slideIndex = n;
+  }
+  
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";  
+  
+  if (x[slideIndex-1]) {
+    x[slideIndex-1].style.display = "block";
+  }
 }
-
 var myIndex = 0;
-carousel();
 
 function carousel() {
   var i;
   var x = document.getElementsByClassName("mySlides");
+  
+  // 隱藏所有幻燈片
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";  
   }
+  
+  // 更新索引並確保它在範圍內
   myIndex++;
-  if (myIndex > x.length) {myIndex = 1}    
-  x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  if (myIndex > x.length) {
+    myIndex = 1;
+  }
+  
+  // 顯示當前幻燈片
+  if (x[myIndex-1]) {
+    x[myIndex-1].style.display = "block";
+  }
+  
+  // 設置定時器以每2秒調用一次 carousel 函數
+  setTimeout(carousel, 2000);
 }
 
+// 初始化調用
+carousel();
 
 
 
